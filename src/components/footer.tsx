67eec90c8c778ac5cbb268018f8829a160ad697b
mohamadhuashiksherali <mohamadhuashik.sherali@dci.in>
2025-09-17 17:26:55 +0530

    footer changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -79,12 +79,12 @@ export default function Footer() {
             Address: Pamban Swamy Nagar, Thiruparankundram, Tamil Nadu 625005
           </p>
 
-          <p className="text-base mt-10">
+          {/* <p className="text-base mt-10">
             The Microsoft CMT service was used for managing the peer-reviewing
             process for this conference. This service was provided for free by
             Microsoft and they bore all expenses, including costs for Azure
             cloud services as well as for software development and support.
-          </p>
+          </p> */}
         </div>
 
         {/* Right - Map */}
